e7f8b4215bc8174cc615439642ee559cdaf5b6a4
sdelamo <sergio.delamo@softamo.com>
2019-04-19 08:09:39 +0200

    0.21

--- src/main/java/example/micronaut/gateway/Application.java
+++ src/main/java/example/micronaut/gateway/Application.java
@@ -5,6 +5,7 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("MICRONAUT_HTTP_SERVICES_BOOKS_URL=" + System.getenv("MICRONAUT_HTTP_SERVICES_BOOKS_URL"));
         Micronaut.run(Application.class);
     }
 }
